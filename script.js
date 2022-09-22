c599a4b774a4f55721846402585f1ddea42d1ab4
Ozan Yalçıntürk <lettoozan08@gmail.com>
2022-09-22 17:32:50 +0300

    update

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const phone = document.getElementById('phone');
 const password = document.getElementById('password');
 const re_password = document.getElementById('re-password');
 
-// function error(input) {
-//     input.className = 'form-control is-invalid'
-// }
-// function success(input) {
-//     input.className = 'form-control is-valid'
-// }
 
 username.addEventListener('input', user_validate);
 email.addEventListener('input', email_validate);
@@ -18,87 +12,92 @@ phone.addEventListener('input', phone_validate);
 password.addEventListener('input', password_validate);
 re_password.addEventListener('input', re_password_validate);
 
-
+var true_username;
 
 function user_validate(e) {
+    console.log('username')
     //Username
     if(username.value.length <= 3 || username.value.indexOf(" ") != -1) {
         username.className = ('form-control is-invalid');
-        console.log(username.value.indexOf(" "));
+        document.getElementById('username_error').innerHTML = 'En az 3 harf olmalı, boşluk kullanılmamamalı';
     }
     else {
         username.className = ('form-control is-valid');
-        console.log(username.value.indexOf(" "));
+        document.getElementById('username_error').innerHTML = null;
     }
+    true_username = username.className;
 }
 function email_validate(e) {
+    console.log(true_username);
     //Email
     if(email.value.length <= 3 || email.value.indexOf(" ") != -1 || email.value.indexOf('@') === -1 || email.value.indexOf('.com') === -1) {
         email.className = ('form-control is-invalid');
-        console.log(email.value);
+        document.getElementById('email_error').innerHTML = 'Lütfen Geçerli bir Mail girin';
     }
     else {
         email.className = ('form-control is-valid');
-        console.log(email.value);
+        document.getElementById('email_error').innerHTML = null;
     }
 }
 function phone_validate(e) {
     //Phone
     if(phone.value.length != 10 || phone.value.indexOf(" ") != -1) {
         phone.className = ('form-control is-invalid');
-        console.log(phone.value.length);
+        document.getElementById('phone_error').innerHTML = '';
     }
     else {
         phone.className = ('form-control is-valid');
-        console.log(phone.value.length);
+        document.getElementById('phone_error').innerHTML = null;
     }
 }
 function password_validate(e) {
     //password
     if(password.value.length <= 3 || password.value.indexOf(" ") != -1) {
         password.className = ('form-control is-invalid');
+        document.getElementById('password_error').innerHTML = 'En az 3 harf olmalı, boşluk kullanılmamamalı';
     }
     else {
         password.className = ('form-control is-valid');
+        document.getElementById('password_error').innerHTML = null;
     }
 }
 function re_password_validate(e) {
     //re-password
-    if(password.value != re_password.value) {
+    if(password.value != re_password.value || re_password.value.length <= 3) {
         re_password.className = ('form-control is-invalid');
+        document.getElementById('re-password_error').innerHTML = 'En az 3 harf olmalı, boşluk kullanılmamamalı';
     }
     else {
         re_password.className = ('form-control is-valid');
+        document.getElementById('re-password_error').innerHTML = null;
+        //ex.push("re")
     }
 }
+console.log(true_username);
+if(true_username == ('form-control is valid')) {
+    function alert_button() {
+        alert("Doğru");
+    }
+}else {
+    function alert_button() {
+        alert("Yanlış");
+    }
+}
+//console.log(ex);
+// const ex = [(username).value.indexOf(""),(email).value.indexOf("")]
 
+// if (ex[0]===-1){
+//     console.log("passeddd");
+// }
+// if(ex)
 
-form.addEventListener('input', function(e) {
-    e.preventDefault();
-    
-    // if(username.value === '') {
-    //     error(username);
-    // }else {
-    //     success(username)
-    // }
-    // if(email.value === '') {
-    //     error(email);
-    // }else {
-    //     success(email);
-    // }
-    // if(phone.value === '') {
-    //     error(phone);
-    // }else {
-    //     success(phone);
-    // }
-    // if(password.value === '') {
-    //     error(password);
-    // }else {
-    //     success(password);
-    // }
-    // if(re_password.value === '') {
-    //     error(re_password);
-    // }else {
-    //     success(re_password);
-    // }
-});
+// if ((username, email, phone, password, re_password).className == ('form-control is-valid')) {
+//     function alert_button() {
+//         alert("Doğru");
+//     }
+// }else {
+//     function alert_button() {
+//         alert("Yanlış");
+//     }
+// }
+
